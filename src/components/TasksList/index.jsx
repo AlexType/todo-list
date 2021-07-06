ced7da198433b946da676b7b0161d5b7c0e15a1c
Alexandr Bryzgalov <arurulras@icloud.com>
2021-07-07 09:54:20 +1000

    added: tracking the availability of tasks

--- src/components/TasksList/index.jsx
+++ src/components/TasksList/index.jsx
@@ -1,14 +1,29 @@
+import { Empty, Button } from 'antd';
+import { useRouter } from 'next/router';
 import TaskItem from "../TaskItem";
 
 export default function TasksList({ tasks }) {
+    const router = useRouter();
     return (
         <div className="row row-cols-1 gy-3">
-            {tasks.map(item => <TaskItem
-                id={item.id}
-                key={item.id}
-                title={item.title}
-                isCompleted={item.isCompleted}
-            />)}
+            {
+                tasks.length ?
+                    tasks.map(item => <TaskItem
+                        id={item.id}
+                        key={item.id}
+                        title={item.title}
+                        isCompleted={item.isCompleted}
+                    />)
+                    :
+                    <Empty
+                        image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+                        imageStyle={{ height: 160 }}
+                        description={<span>У вас пока нет задач</span>} >
+                        <Button type="primary" onClick={() => router.push('/add-task')}>
+                            Создать задачу
+                        </Button>
+                    </Empty>
+            }
         </div>
     )
 }
