403ac27f943c5d752aea1a9930697ef5a57deace
Alexandr Bryzgalov <arurulras@icloud.com>
2021-07-08 00:58:11 +1000

    upd: index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Home() {
     return (
         <div className="container">
-            <h1 className="title-page mb-4">Главная</h1>
+            <h1 className="title-page mb-3">Главная</h1>
+            <p><span className="logo">TodoList</span> - это небольшой сервис, который может отслеживать/редактировать поставленные задачи.</p>
+            <p>Сервис не использует back-end часть для сохранения задач, вместо этого задачи сохраняются в localStorage вашего браузера.</p>
         </div>
     );
 }
