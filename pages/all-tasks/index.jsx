90853895e14529c975d1d1c13b92f6565e70cad4
Alexandr Bryzgalov <arurulras@icloud.com>
2021-07-07 11:14:59 +1000

    added: quick task addition

--- pages/all-tasks/index.jsx
+++ pages/all-tasks/index.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import { useSelector } from "react-redux"
-import TaskItem from "../../src/components/TaskItem";
+import { Modal, Button } from 'antd';
 import TasksList from "../../src/components/TasksList";
+import TaskAdd from "../../src/components/TaskAdd";
 
 export default function AllTasks() {
+
+    const [visible, setVisible] = useState(false);
+
+    const showModal = () => {
+        setVisible(true);
+    };
+
+    const handleCancel = () => {
+        setVisible(false);
+    };
 
     const tasks = useSelector(state => state.tasks.tasks);
 
@@ -10,6 +22,20 @@ export default function AllTasks() {
         <div className="container">
             <h1>Все задачи</h1>
             <TasksList tasks={tasks} />
+            <Button className="mt-4" type="primary" onClick={showModal}>
+                Добавить задачу
+            </Button>
+            <Modal
+                title="Быстрое добавление"
+                visible={visible}
+                footer={[
+                    <Button key="submit" type="primary" onClick={handleCancel}>
+                        Закрыть
+                    </Button>
+                ]}
+            >
+                <TaskAdd />
+            </Modal>
         </div>
     )
 }
