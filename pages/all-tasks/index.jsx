06f680c733583afb12738bf7aad719ccf7c759ca
Alexandr Bryzgalov <arurulras@icloud.com>
2021-07-11 21:10:21 +1000

    added: filters for tasks

--- pages/all-tasks/index.jsx
+++ pages/all-tasks/index.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
 import Head from "next/head";
+import moment from "moment";
 import { useSelector } from "react-redux";
 import { Modal, Button } from "antd";
 import TasksList from "../../src/components/TasksList";
 import TaskAdd from "../../src/components/TaskAdd";
+import Filters from "../../src/components/Filters";
 
 export default function AllTasks() {
 
     const [visible, setVisible] = useState(false);
+    const [filters, setFilters] = useState(null);
     const tasks = useSelector(state => state.tasks.tasks);
+
+    const filterByFinished = (item) => item.finished !== null;
+    const filterByActive = (item) => !item.isCompleted;
+    const filterByDeadline = (item) => item.deadline !== null && !item.isCompleted && moment() > moment(item.deadline);
+
+    const filteredList = () => {
+        if (filters === "finished") return tasks.filter(filterByFinished);
+        else if (filters === "active") return tasks.filter(filterByActive);
+        else if (filters === "deadline") return tasks.filter(filterByDeadline);
+        else return tasks;
+    };
 
     return (
         <div className="container">
@@ -16,7 +30,12 @@ export default function AllTasks() {
                 <title>Все задачи</title>
             </Head>
             <h1 className="title-page mb-4">Все задачи</h1>
-            <TasksList tasks={tasks} />
+            <div className="row justify-content-end mb-4">
+                <div className="col-auto">
+                    <Filters setFilters={setFilters} />
+                </div>
+            </div>
+            <TasksList tasks={filteredList()} />
             <Button className="btn-success mt-4" onClick={() => setVisible(true)}>
                 Добавить задачу
             </Button>
@@ -28,6 +47,6 @@ export default function AllTasks() {
             >
                 <TaskAdd />
             </Modal>
-        </div>
+        </div >
     );
 }
---
+++ src/components/Filters/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Dropdown, Menu } from "antd";
+
+export default function Filters({ setFilters }) {
+
+    const menu = (
+        <Menu>
+            <Menu.Item key="1" onClick={() => setFilters(null)}>
+                <span>Все</span>
+            </Menu.Item>
+            <Menu.Item key="2" onClick={() => setFilters("finished")}>
+                <span>
+                    Выполненые
+                </span>
+            </Menu.Item>
+            <Menu.Item key="3" onClick={() => setFilters("active")}>
+                <span>
+                    Активные
+                </span>
+            </Menu.Item>
+            <Menu.Item key="4" onClick={() => setFilters("deadline")}>
+                <span>
+                    Просрочены
+                </span>
+            </Menu.Item>
+        </Menu>
+    );
+
+    return (
+        <Dropdown overlay={menu}>
+            <div className="filter-item">
+                <i className="bi bi-funnel-fill"></i>
+                <span>Фильтр</span>
+            </div>
+        </Dropdown>
+    );
+}
+
+Filters.propTypes = {
+    setFilters: PropTypes.func,
+};
