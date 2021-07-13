e713b7cdc70a7e90aaf3fa00fa711e82e1dc0da1
Alexandr Bryzgalov <arurulras@icloud.com>
2021-07-13 13:58:04 +1000

    upd: TaskList added adaptive

--- src/components/TasksList/index.jsx
+++ src/components/TasksList/index.jsx
@@ -5,7 +5,7 @@ import TaskItem from "../TaskItem";
 
 export default function TasksList({ tasks }) {
     return (
-        <article className="row row-cols-4 gy-3">
+        <article className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 gy-3">
             {
                 tasks.length ?
                     tasks.map(item => <TaskItem
